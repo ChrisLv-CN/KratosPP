4ab6fa2b1843b795809a303205deeadaa0083a08
ChrisLv <12737@live.com>
2023-10-03 22:43:07 +0700

    树的死亡动画

---
+++ src/Ext/TerrainDestroyAnim.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include <string>
+#include <vector>
+
+#include <GeneralStructures.h>
+#include <TerrainClass.h>
+
+#include <Common/INI/INI.h>
+#include <Common/INI/INIConfig.h>
+
+#include <Ext/Data/TerrainDestroyAnimData.h>
+#include <Ext/ExpandAnimsManager.h>
+
+namespace TerrainDestroyAnim
+{
+	static void PlayDestroyAnim(TerrainClass* pTerrain)
+	{
+		TerrainDestroyAnimData* data = INI::GetConfig<TerrainDestroyAnimData>(INI::Rules, pTerrain->Type->ID)->Data;
+		if (data->Enable)
+		{
+			CoordStruct location = pTerrain->GetCoords();
+			ExpandAnimsManager::PlayExpandAnims(*data, location);
+		}
+	}
+};
---
+++ src/Hooks/TerrainExtHook.cpp
@@ -0,0 +1,17 @@
+#include <exception>
+#include <Windows.h>
+
+#include <TerrainClass.h>
+
+#include <Extension.h>
+#include <Utilities/Macro.h>
+#include <Extension/TerrainTypeExt.h>
+#include <Ext/TerrainDestroyAnim.h>
+
+DEFINE_HOOK(0x71C94C, TerrainClass_Remove_PlayDestroyAnim, 0xA)
+{
+	GET(TerrainClass*, pTerrain, ESI);
+	TerrainDestroyAnim::PlayDestroyAnim(pTerrain);
+
+	return 0;
+}
