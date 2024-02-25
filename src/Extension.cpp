f795ae0409cd8ca430359eef73a6532c09d98aaa
ChrisLv <12737@live.com>
2024-02-25 18:37:23 +0800

    cancel save_id check

--- src/Extension.cpp
+++ src/Extension.cpp
@@ -248,18 +248,18 @@ DEFINE_HOOK(0x67E826, LoadGame_Ext, 0x6)
 	return 0;
 }
 
-DEFINE_HOOK(0x67D04E, Game_Save_SavegameInformation, 0x7)
-{
-	REF_STACK(SavegameInformation, Info, STACK_OFFSET(0x4A4, -0x3F4));
-	Info.Version = Info.Version + SAVEGAME_ID;
-	return 0;
-}
+// DEFINE_HOOK(0x67D04E, Game_Save_SavegameInformation, 0x7)
+// {
+// 	REF_STACK(SavegameInformation, Info, STACK_OFFSET(0x4A4, -0x3F4));
+// 	Info.Version = Info.Version + SAVEGAME_ID;
+// 	return 0;
+// }
 
-DEFINE_HOOK(0x559F29, LoadOptionsClass_GetFileInfo, 0x8)
-{
-	if (!R->BL()) return 0x55A03D; // vanilla overridden check
+// DEFINE_HOOK(0x559F29, LoadOptionsClass_GetFileInfo, 0x8)
+// {
+// 	if (!R->BL()) return 0x55A03D; // vanilla overridden check
 
-	REF_STACK(SavegameInformation, Info, STACK_OFFSET(0x400, -0x3F4));
-	Info.Version = Info.Version - SAVEGAME_ID;
-	return 0x559F29 + 0x8;
-}
+// 	REF_STACK(SavegameInformation, Info, STACK_OFFSET(0x400, -0x3F4));
+// 	Info.Version = Info.Version - SAVEGAME_ID;
+// 	return 0x559F29 + 0x8;
+// }
