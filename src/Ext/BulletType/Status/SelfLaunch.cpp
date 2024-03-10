7a313e08a921ab5273a49f81fb3b7bc13cefef2e
ChrisLv <12737@live.com>
2024-03-10 17:42:04 +0800

    bugfix

--- src/Ext/BulletType/Status/SelfLaunch.cpp
+++ src/Ext/BulletType/Status/SelfLaunch.cpp
@@ -108,7 +108,7 @@ bool BulletStatus::OnDetonate_SelfLaunch(CoordStruct* pCoords)
 				pSource->Select();
 			}
 		}
-		if (!FallingDown(pSource, Unsorted::LevelHeight, false))
+		if (!FallingDown(pSource, 0, false))
 		{
 			if (pSource->Target)
 			{
