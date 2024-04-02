807a9d632436c919d18eec03657071fcb50e55f5
ChrisLv <12737@live.com>
2024-04-03 01:20:53 +0800

    版本更新到1.3

--- src/Version.h
+++ src/Version.h
@@ -16,7 +16,7 @@
 #define VERSION_MINOR 1
 
 // Indicates minor changes, like vanilla bugfixes, unhardcodings or hacks
-#define VERSION_REVISION 2
+#define VERSION_REVISION 3
 
 // Indicates Kratos-related bugfixes only
 #define VERSION_PATCH 0
